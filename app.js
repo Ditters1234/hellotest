4758992f463273eb553cc310ea7a72a92ed7a7d7
Nicholas Tinsley <Nicholas Tinsley>
2017-03-09 14:58:54 -0600

    Third Commit. Changed Hello World text in app.js back to Hello World

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('This is only a test');
+  res.send('Hello World ');
 });
 
 var server = app.listen(8080, function(){
