8ac6396a232e50617101f3ef45b7dfbd61d831d1
Nicholas Tinsley <Nicholas Tinsley>
2017-03-09 15:30:26 -0600

    Fifth Commit. Changed Hello World text in app.js again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World. I am Diginick.com');
+  res.send('Hello World. I am Diginick.com. This is only a test.');
 });
 
 var server = app.listen(8080, function(){
