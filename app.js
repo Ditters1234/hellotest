39531ea7d66e5aed7315e850b205ab8dab0442a6
Nicholas Tinsley <Nicholas Tinsley>
2017-03-10 17:17:31 -0600

    changing port to 8080.

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
   res.send('Hello World. I am Diginick.com. This is only a test. But at least it passed.');
 });
 
-var server = app.listen(443, function(){
-  console.log('Magic is happening on port 443')
+var server = app.listen(8080, function(){
+  console.log('Magic is happening on port 8080')
 });
